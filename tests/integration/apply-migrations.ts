91cdd5d714baa6f6cbb63fb04a2378c88b55e808
Atin <atin@atin.me>
2026-08-12 15:58:51 -0400

    MRQ-79 use next migration slot after main

--- tests/integration/apply-migrations.ts
+++ tests/integration/apply-migrations.ts
@@ -18,7 +18,7 @@ import coldStartMigrationSql from "../../migrations/0011_cold_start.sql?raw";
 import publicSchedulesMigrationSql from "../../migrations/0011_public_schedules.sql?raw";
 import peopleAnnotationsMigrationSql from "../../migrations/0012_people_annotations.sql?raw";
 import agentEvaluatorSeatsMigrationSql from "../../migrations/0013_agent_evaluator_seats.sql?raw";
-import inboundDeliveryStateMigrationSql from "../../migrations/0013_inbound_delivery_state.sql?raw";
+import inboundDeliveryStateMigrationSql from "../../migrations/0014_inbound_delivery_state.sql?raw";
 import type { Env } from "../../src/index";
 import { WIPE_ORDER } from "../../src/lib/reset-demo/reseed-demo";
 
